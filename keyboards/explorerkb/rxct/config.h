34da9aa92091aa7ae454423e7a5cbf3ce49af536
DreaM117er <107366200+DreaM117er@users.noreply.github.com>
2023-05-05 04:05:41 +0800

    Explorer Keyboard firmware

---
+++ keyboards/explorerkb/rxct/config.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2023 @DreaM117er
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+/* Key matrix configuration. */
+#define MATRIX_ROW_PINS \
+    { F7, B1, B3, B2, B6 }
+#define MATRIX_COL_PINS \
+    { D4, C6, D7, E6, B4, B5 }
+
+/* Handedness. */
+#define MASTER_LEFT
+
+#define DIODE_DIRECTION ROW2COL
+
