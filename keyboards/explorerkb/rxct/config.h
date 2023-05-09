da5cefe26c2fbede8885ae2c3b4301f690da5a32
DreaM117er <107366200+DreaM117er@users.noreply.github.com>
2023-05-10 02:23:32 +0800

    Add files via upload

--- keyboards/explorerkb/rxct/config.h
+++ keyboards/explorerkb/rxct/config.h
@@ -17,11 +17,21 @@
 
 #pragma once
 
+/* key matrix size */
+// Rows are doubled-up
+#define MATRIX_ROWS 10
+#define MATRIX_COLS 6
+
 /* Key matrix configuration. */
 #define MATRIX_ROW_PINS \
     { F7, B1, B3, B2, B6 }
 #define MATRIX_COL_PINS \
     { D4, C6, D7, E6, B4, B5 }
+
+#define SOFT_SERIAL_PIN D2
+
+/* Set 0 if debouncing isn't needed */
+#define DEBOUNCE 5
 
 /* Handedness. */
 #define MASTER_LEFT
