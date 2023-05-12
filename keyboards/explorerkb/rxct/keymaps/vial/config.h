db79443ab64cb422bb9a7360ce506ef9ce27ee59
DreaM117er <107366200+DreaM117er@users.noreply.github.com>
2023-05-12 16:21:59 +0800

    Add files via upload

---
+++ keyboards/explorerkb/rxct/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x18, 0x52, 0xA6, 0xC5, 0xCD, 0xE7, 0xBC, 0x6B}
+#define VIAL_UNLOCK_COMBO_ROWS {3, 5}
+#define VIAL_UNLOCK_COMBO_COLS {8, 5}
