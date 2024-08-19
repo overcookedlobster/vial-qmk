c9c46310d733ad14790809171b71d2606ba1cf25
DreaM117er <107366200+DreaM117er@users.noreply.github.com>
2024-08-20 01:25:12 +0800

    fix openero65 code to new version

--- keyboards/explorerkb/openero65/config.h
+++ keyboards/explorerkb/openero65/config.h
@@ -17,18 +17,6 @@
 
 #pragma once
 
-/* key matrix size */
-#define MATRIX_ROW_PINS \
-    { GP13, GP12, GP11, GP10, GP9 }
-#define MATRIX_COL_PINS \
-    { GP0, GP1, GP2, GP3, GP4, GP5, GP6, GP7, GP14, GP29, GP28, GP27, GP26, GP15, GP8 }
-
-// Rows are doubled-up
-#define MATRIX_ROWS 5
-#define MATRIX_COLS 15
-
 /* Set 0 if debouncing isn't needed */
 #define DEBOUNCE 5
-
-#define DIODE_DIRECTION COL2ROW
 
