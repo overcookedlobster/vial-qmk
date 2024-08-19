8b5f1914209bd0eb9612967d7daba4fd252bf459
DreaM117er <107366200+DreaM117er@users.noreply.github.com>
2024-08-20 01:28:33 +0800

    Delete keyboards/explorerkb/elice directory

--- keyboards/explorerkb/elice/keymaps/vial/config.h
+++
@@ -1,7 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0-or-later */
-
-#pragma once
-
-#define VIAL_KEYBOARD_UID {0x7F, 0x73, 0x47, 0xEA, 0x43, 0x6C, 0xAF, 0xA3}
-#define VIAL_UNLOCK_COMBO_ROWS {4, 4}
-#define VIAL_UNLOCK_COMBO_COLS {4, 7}
