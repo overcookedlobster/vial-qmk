385e690deca15fa98b680550c8f11f5c8a0df184
flyinglobster <yahwistasalomo@gmail.com>
2024-09-30 22:13:58 +0700

    gans

--- keyboards/explorerkb/blank/keymaps/v2/keymap.c
+++ keyboards/explorerkb/blank/keymaps/v2/keymap.c
@@ -100,9 +100,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [DEFAULT] = LAYOUT(
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    LGUI_T(SEND_STRING("1234qwer")),
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    LGUI_T(KC_DEL),
         LT(LOWER,KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,             KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, LT(WORKSPACE,KC_QUOT),
-        LSFT_T(KC_TILD),  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, LSFT_T(KC_BSLS),
+        LSFT_T(KC_GRV),  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, LSFT_T(KC_BSLS),
         KC_LCTL, KC_BTN1, LT(SHORTCUTS,KC_BTN1), KC_LALT, ALT_T(KC_DEL), LGUI_T(KC_ENT), ENC_MODE , LT(SHORTCUTS,KC_RSFT), KC_SPC, LT(RAISE,KC_BSPC), ALT_T(KC_BSLS), KC_BTN2, KC_BTN2, KC_RALT
     ),
 
